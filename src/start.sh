61f9b3ba0a282a8afea541d8bc548d2ed6648969
Sergio Rodenas <sergiorodenasgomez@gmail.com>
2024-08-22 01:55:40 +0200

    Try

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --ckpt /model.safetensors --port 3000 &
+python /stable-diffusion-webui/webui.py --xformers --skip-python-version-check --skip-torch-cuda-test --ckpt /model.safetensors --port 3000 &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
