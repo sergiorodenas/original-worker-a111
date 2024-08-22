2d411739906e99cf66fdd6959b5947728d52eeba
Sergio Rodenas <sergiorodenasgomez@gmail.com>
2024-08-22 02:21:56 +0200

    Try

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,9 @@
 
 echo "Worker Initiated"
 
+echo "Launching SD"
+python -u /stable-diffusion-webui/launch.py
+
 echo "Starting WebUI API"
 python -u /stable-diffusion-webui/webui.py --xformers --listen --skip-python-version-check --skip-torch-cuda-test --ckpt /model.safetensors --port 3000 --api &
 
